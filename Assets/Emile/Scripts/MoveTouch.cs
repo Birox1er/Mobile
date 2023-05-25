e13cccf4ddb50901831575466326da6db0bc718f
Emile Birraux <Emile.birraux@edu.devinci.fr>
2023-05-25 14:05:22 +0200

    chara start

--- Assets/Emile/Scripts/MoveTouch.cs
+++ Assets/Emile/Scripts/MoveTouch.cs
@@ -6,6 +6,7 @@ public class MoveTouch : MonoBehaviour
 {
     private Touch _touch;
     private bool _selected;
+    private bool _onTile;
     // Update is called once per frame
     void Update()
     {
@@ -13,7 +14,7 @@ public class MoveTouch : MonoBehaviour
         Vector2 pos = Camera.main.ScreenToWorldPoint(_touch.position);
         Debug.Log(pos);
         Debug.Log(transform.position);
-        if (_touch.phase == TouchPhase.Began && transform.position.x + 0.5 >= pos.x && pos.x>= transform.position.x - 0.5 && transform.position.y+0.5 >= pos.y && pos.y >= transform.position.y-0.5) 
+        if (_touch.phase == TouchPhase.Began && transform.position.x + 0.5 >= pos.x && pos.x >= transform.position.x - 0.5 && transform.position.y + 0.5 >= pos.y && pos.y >= transform.position.y - 0.5)
         {
             _selected = true;
         }
@@ -21,9 +22,17 @@ public class MoveTouch : MonoBehaviour
         {
             transform.position = pos;
         }
-        if(_touch.phase== TouchPhase.Ended|| _touch.phase == TouchPhase.Canceled)
+        if (_touch.phase == TouchPhase.Ended)
+        {
+            if (_onTile == true)
+            {
+                //fnc pour poser carte
+            }
+        }
+        if (_touch.phase == TouchPhase.Ended || _touch.phase == TouchPhase.Canceled)
         {
             _selected = false;
+            _onTile = false;
         }
     }
 }
