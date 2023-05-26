3b07cc9cc80f3ba89e7f4f568e6f03059ca7a3b5
riufix <enguerrand.cousin@gmail.com>
2023-05-26 13:33:14 +0200

    [Fix]: UI

---
+++ Assets/Enguerrand/UI/CanvaManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.Android;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class CanvaManager : MonoBehaviour
+{
+    [Header("pause menu")]
+    [SerializeField] private GameObject _pauseMenuUI;
+    [SerializeField] private bool _gamePaused = false;
+
+    [Header("turn")]
+    [SerializeField] private TextMeshProUGUI _turnNumber;
+    [SerializeField] private GameObject _nextBtn;
+    [SerializeField] private bool _canGoNextRound = true;
+    [SerializeField] private int _currentRound;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if (_canGoNextRound)
+        {
+            //if the player touch the Next button on the screen
+            _currentRound++;
+            _turnNumber.text = "Turn : " + _currentRound;
+        }
+    }
+
+
+    void Pause()
+    {
+        _pauseMenuUI.SetActive(false);
+        _canGoNextRound = false;
+        Time.timeScale = 0;
+        _gamePaused = true;
+    }
+}
+
