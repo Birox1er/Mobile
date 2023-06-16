733f42fbeb80da539d473e1c4ed786335d11cd58
Haethor <aym.mougeot@gmail.com>
2023-06-16 07:00:07 +0200

    Reset and scenes

--- Assets/Emile/Scripts/SceneLoader.cs
+++ Assets/Emile/Scripts/SceneLoader.cs
@@ -5,11 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    GameObject levelManager;
+
+    public void Awake()
+    {
+        levelManager = GameObject.Find("LevelManager");
+    }
+    public void LoadSameScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    
+    public void LoadNextScene()
+    {
+        if (levelManager != null)
+        {
+            levelManager.GetComponent<LevelManager>().UnlockLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
     //load the scene dragged in the inspector
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
